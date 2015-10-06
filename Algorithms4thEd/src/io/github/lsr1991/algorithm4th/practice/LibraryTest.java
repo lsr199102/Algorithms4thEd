52abfcdde88b9da23739f608f41187659a9fe75d
林澍荣 <linshurong@kuanguang.local>
2015-10-06 11:48:13 +0800

    init

---
+++ Algorithms4thEd/src/io/github/lsr1991/algorithm4th/practice/LibraryTest.java
@@ -0,0 +1,21 @@
+package io.github.lsr1991.algorithm4th.practice;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class LibraryTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N = 100;
+		StdDraw.setXscale(0, N);
+		StdDraw.setYscale(0, N*N);
+		StdDraw.setPenRadius(.01);
+		for (int i = 1; i <= N; i ++) {
+			StdDraw.point(i, i);
+			StdDraw.point(i, i*i);
+			StdDraw.point(i, i*Math.log(i));
+		}
+	}
+
+}
