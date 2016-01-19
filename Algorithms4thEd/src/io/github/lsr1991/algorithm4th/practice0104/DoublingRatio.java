c9e5f23fd58219aa27ea2e4d1c6f1b87f890d921
林澍荣 <linshurong@kuanguang.local>
2016-01-19 21:32:53 +0800

    ShellSortTest

--- Algorithms4thEd/src/io/github/lsr1991/algorithm4th/practice0104/DoublingRatio.java
+++ Algorithms4thEd/src/io/github/lsr1991/algorithm4th/practice0104/DoublingRatio.java
@@ -1,30 +1,54 @@
 package io.github.lsr1991.algorithm4th.practice0104;
 
+import io.github.lsr1991.algorithm4th.practice0201.Insertion;
+import io.github.lsr1991.algorithm4th.practice0201.Selection;
+import io.github.lsr1991.algorithm4th.practice0201.Shell;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.Stopwatch;
 
 public class DoublingRatio {
 
-	public static double timeTrial(int N) {
+	public static enum InputType {
+		RANDOM,
+		ASCEND,
+		DESCEND
+	};
+	
+	public static double timeTrial(int N, InputType type) {
 		int MAX = 1000000;
-		int[] a = new int[N];
+//		int[] a = new int[N];
+		Integer[] a = new Integer[N];
 		for (int i = 0; i < N; i ++) {
-			a[i] = StdRandom.uniform(-MAX, MAX);
+			switch(type) {
+			case RANDOM: a[i] = StdRandom.uniform(-MAX, MAX);break;
+			case ASCEND: a[i] = i;break;
+			case DESCEND: a[i] = N - i;break;
+			default: a[i] = StdRandom.uniform(-MAX, MAX);
+			}
 		}
 		Stopwatch timer = new Stopwatch();
-		int cnt = ThreeSumFast.count(a);
+		Shell.sort(a);
+//		int cnt = ThreeSumFast.count(a);
 		return timer.elapsedTime();
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double prev = timeTrial(125);
-		for (int N = 250; N < 100000; N += N) {
-			double time = timeTrial(N);
-			StdOut.printf("%6d %7.1f ", N, time);
-			StdOut.printf("%5.1f\n", time/prev);
-			prev = time;
+		if (args.length < 1) {
+			StdOut.println("Usage: <input max scale>");
+			System.exit(1);
+		}
+		int maxScale = Integer.parseInt(args[0]); 
+		for (InputType type: InputType.values()) {
+			StdOut.println(type);
+			double prev = timeTrial(125, type);
+			for (int N = 250; N < maxScale; N += N) {
+				double time = timeTrial(N, type);
+				StdOut.printf("%6d %7.1f ", N, time);
+				StdOut.printf("%5.1f\n", time/prev);
+				prev = time;
+			}
 		}
 	}
 
